46c5db934abf2c98c9ca70fd98a486c6075b24c6
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-03-09 16:27:16 +0100

    update

--- tac-site/stock.php
+++ tac-site/stock.php
@@ -9,3 +9,20 @@ echo "| Petite(s) noisette(s)    |  $smallNuts   | ".PHP_EOL;
 echo "| Noisette(s) classique(s) |  $regularNuts     | ".PHP_EOL;
 echo "| Grosse(s) noisette(s)    |  $bigNuts    | ".PHP_EOL;
 echo "-------------------------------------";
+
+var_dump($smallNuts);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <div id="tac">
+        <?php echo   $regularNuts ?>
+    </div>
+</body>
+</html>
