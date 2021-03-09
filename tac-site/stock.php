8aff00e71addead92888d4bd304e98b223dd18cd
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-03-09 15:45:18 +0100

    update tac

--- tac-site/stock.php
+++ tac-site/stock.php
@@ -3,6 +3,9 @@ $smallNuts = 125;
 $regularNuts = 2;
 $bigNuts = 13;
 
-echo 'Le stock de Tac';
-echo "-----------------";
-echo "| Petite(s) noisette(s)   |  $smallNuts";
+echo 'Le stock de Tac'.PHP_EOL;
+echo '-------------------------------------'.PHP_EOL;
+echo "| Petite(s) noisette(s)    |  $smallNuts   | ".PHP_EOL;
+echo "| Noisette(s) classique(s) |  $regularNuts     | ".PHP_EOL;
+echo "| Grosse(s) noisette(s)    |  $bigNuts    | ".PHP_EOL;
+echo "-------------------------------------";
